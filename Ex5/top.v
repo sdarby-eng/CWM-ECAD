9741234ff52f2b45affcc1c901b43a0003f9eb27
Cloud User <centos@ip-172-31-12-1.eu-west-2.compute.internal>
2021-06-08 14:42:15 +0000

    A completed module for Ex5

--- Ex5/top.v
+++ Ex5/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #5 - Air Conditioning
-// Student Name:
-// Date: 
+// Student Name: Stephen Darby	
+// Date: 8/6/21
 //
 //  Description: In this exercise, you need to an air conditioning control system
 //  According to the state diagram provided in the exercise.
@@ -12,4 +12,53 @@
 //  outputs:
 //           heating, cooling
 //////////////////////////////////////////////////////////////////////////////////
+
+`timescale 1ns/ 100ps 
+
+module ac(input clk, 
+	input [4:0] temperature, 
+	output reg heating, 
+	output reg cooling); 
+  
+//The transition values
+parameter coolOn = 22; 
+parameter heatOn = 18; 
+parameter idleOn = 20;
+
+
+always @(posedge clk)
+begin
+	if(heating) //In the heating state (or both)
+	begin
+		if(temperature>=idleOn)
+		begin
+			heating<=0;
+			cooling<=0;
+		end
+	end
+	else if (cooling) //In the cooling state (or both)
+	begin
+		if(temperature<=idleOn)
+		begin
+			cooling<=0;
+			heating<=0;
+		end
+	end
+	else //In the idle state
+	begin
+		if(temperature<=heatOn)
+		begin
+			heating<=1;
+			cooling<=0;
+		end
+		else if(temperature>=coolOn)
+		begin
+			cooling<=1;
+			heating<=0;
+		end
+	end
+end
+
+
+endmodule
 
