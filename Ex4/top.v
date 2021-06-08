a55fe7c3f670e1ce3acc749eb2fee895ff4fc608
Cloud User <centos@ip-172-31-12-1.eu-west-2.compute.internal>
2021-06-08 11:19:08 +0000

    A mostly complete solution to Ex4

--- Ex4/top.v
+++ Ex4/top.v
@@ -1,7 +1,7 @@
 //////////////////////////////////////////////////////////////////////////////////
 // Exercise #4 - Dynamic LED lights
-// Student Name:
-// Date: 
+// Student Name: Stephen Darby
+// Date: 8/6/2021
 //
 //  Description: In this exercise, you need to design a LED based lighting solution, 
 //  following the diagram provided in the exercises documentation. The lights change 
@@ -15,4 +15,32 @@
 //
 //  You need to write the whole file.
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
 
+module LEDLights(
+	input rst,
+	input clk,
+	input button,
+	output [2:0] colour
+	);
+
+	reg [2:0] colour; 
+	//Everything is held within this loop
+	always @ (posedge clk) 
+	begin
+		if(rst) 
+		begin
+			colour = 3'b001;
+		end
+		
+		else 
+		begin
+			if(button) 
+				colour = colour + 3'b001;	
+		end
+
+		if ((colour== 3'b000)||(colour==3'b111)) 
+			colour = 3'b001;
+	end
+
+endmodule
--- Ex4/top_tb.v
+++ Ex4/top_tb.v
@@ -6,4 +6,99 @@
 // Description: A testbench module to test Ex4 - Dynamic LED lights
 // You need to write the whole file
 //////////////////////////////////////////////////////////////////////////////////
+`timescale 1ns / 100ps
 
+module top_tb();
+
+	parameter CLK_PERIOD = 10;
+	parameter PAUSE = 3*CLK_PERIOD;
+  
+
+    //Initialising regitsers and wires
+    reg clk;
+    reg rst;    
+    reg button;    
+    wire [2:0] colour;
+    reg [2:0] colour_prev_state;
+    reg err;
+
+    //Clock generation
+	initial 
+	begin
+		clk = 1'b0;
+        	forever
+         		#(CLK_PERIOD/2) clk=~clk;
+    	end
+
+	//Testing
+
+	initial 
+	begin 
+		rst = 1;        
+		err = 0;
+		button = 1;;
+		colour_prev_state = 0;
+
+		//Reset Testing
+		#PAUSE
+		if (colour != 3'b001) 
+		begin
+		    err = 1;
+		    $display("***TEST FAILED! Reset maps to 1!***");   
+		end
+
+		rst = 0;
+		#PAUSE
+		if (colour == 'b001) 
+		begin
+			#PAUSE
+			if (colour == 'b001) 
+			begin
+		   		 err = 1;
+		   		 $display("***TEST FAILED! Reset is off, so colour should change!***");   
+	      		end   
+		end
+
+		//Button Testing
+		#PAUSE
+		button = 0;
+		colour_prev_state = colour;
+		#PAUSE
+		if (colour != colour_prev_state) 
+		begin
+		    err = 1;
+		    $display("***TEST FAILED! The button should stop incrementing things!***");   
+		end
+	
+		//Testing for illegal states forever
+		forever
+		begin
+			#PAUSE
+			if (colour == 3'b111 || colour == 3'b000) 
+			begin 
+           			 err = 1;
+           			 $display("***TEST FAILED! Illegal State Reached!***");
+			end        
+        	end  
+	end
+
+
+
+
+
+    //If no error flags, raised, success!
+    initial begin
+        #1000
+        if (err==0)
+            $display("***TEST PASSED! :) ***");
+        $finish;
+    end
+
+    LEDLights top(
+        .clk (clk),
+	 .rst (rst),
+        .button(button),
+        .colour(colour)
+    );
+
+endmodule
